462ba613ce1c6b4c6902d631a27d30c6ba89852e
moscOVER <gustavomoscoso2004@gmail.com>
2025-06-07 02:04:32 -0500

    Uso de las Listas Circulares doblemente enlazdas enseniadas en clase | se las usa para referencia porque el encargado de las listas es mi otro companiero

---
+++ Proyecto/src/main/java/com/mycompany/proyecto/NodoDobleCircular.java
@@ -0,0 +1,14 @@
+///BORRARRRRRRRR
+package com.mycompany.proyecto;
+
+public class NodoDobleCircular<T>{
+        T dato;
+    public NodoDobleCircular<T> siguiente;
+    public NodoDobleCircular<T> anterior;
+    
+    public NodoDobleCircular(T dato) {
+        this.dato = dato;
+        this.siguiente = this;
+        this.anterior = this;
+    }
+}
