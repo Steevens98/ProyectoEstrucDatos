b07428e8f54a597dae39eb784e04a96aa98ed310
Steevens98 <mscueva@espol.edu.ec>
2025-06-07 17:28:53 -0500

    Cambios mainViewController

--- Grupo_07/src/main/java/com/mycompany/proyecto/MainViewController.java
+++ Grupo_07/src/main/java/com/mycompany/proyecto/MainViewController.java
@@ -16,17 +16,15 @@ import javafx.scene.Parent;
 public class MainViewController{
     
     @FXML
-    private void onCreatePersonContact() throws IOException {
-        App.tipoContactoSeleccionado = "persona";
+    private void switchCreateContac() throws IOException {
         App.setRoot("secondary");
     }
 
     @FXML
-    private void onCreateCompanyContact() throws IOException {
-        App.tipoContactoSeleccionado = "empresa";
+    private void switchToContacts() throws IOException {
         App.setRoot("secondary");
     }
-
+    
     
 }
     
