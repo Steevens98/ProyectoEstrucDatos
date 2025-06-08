a5f8ce4a2f457417e20e7672bcf7dcc313faa020
IsaacVra <isakvera@espol.edu.ec>
2025-06-07 21:07:20 -0500

    Se añadio un comentario

--- Grupo_07/src/main/java/Modelo/ImagenEscritor.java
+++ Grupo_07/src/main/java/Modelo/ImagenEscritor.java
@@ -8,11 +8,12 @@ import java.io.File;
 
 public class ImagenEscritor {
 
-
+//Aqui estoy creando un nuevo archivo
     public static void objetoAArchivo(byte[] datos, String rutaSalida) throws IOException {
         try (FileOutputStream fos = new FileOutputStream(new File(rutaSalida))) {
             fos.write(datos);
         }
     }
 }
+
 
