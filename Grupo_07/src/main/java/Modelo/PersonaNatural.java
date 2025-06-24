3d41a6afc8babb385fe546dfd47ec89fac0da803
moscOver <gustavomoscoso2004@gmail.com>
2025-06-23 20:18:44 -0500

    cambios en la clase PersonaNatural

--- Grupo_07/src/main/java/Modelo/PersonaNatural.java
+++ Grupo_07/src/main/java/Modelo/PersonaNatural.java
@@ -1,21 +1,57 @@
 
 package Modelo;
 
+/**
+ *
+ * @author Usuario
+ */
+/**
+ * Clase PersonaNatural. Representa una entidad del sistema.
+ */
 public class PersonaNatural extends Contacto{
-    
-    public PersonaNatural(String nombre, String telefono, String email, String pais, String apellido, String FechaNacimiento) {
-        super(nombre,telefono,email,pais);
-        // Asigna valores a los atributos
-        editarAtributo(TipoAtributo.APELLIDO, apellido);
-        editarAtributo(TipoAtributo.FECHA_NACIMIENTO, FechaNacimiento);
+    private String apellido;
+    private String fechaNacimiento;
+
+    public PersonaNatural(String id,String nombre, String apellido, String telefono, String email, String pais, String fechaNacimiento) {
+        super(id,nombre, telefono, email, pais);
+        this.apellido = apellido;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+/**
+ * Método getApellido. Implementa una operación específica.
+ */
+    public String getApellido() {
+        return apellido;
+    }
+
+/**
+ * Método setApellido. Implementa una operación específica.
+ */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+/**
+ * Método getFechaNacimiento. Implementa una operación específica.
+ */
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+/**
+ * Método setFechaNacimiento. Implementa una operación específica.
+ */
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
     }
 
     @Override
-    protected void inicializarAtributosEspecificos() {
-       // atributos.put(TipoAtributo.APELLIDO, null);
-       // atributos.put(TipoAtributo.FECHA_NACIMIENTO, null);
-        atributos.put(TipoAtributo.APELLIDO, new Atributo(TipoAtributo.APELLIDO, null));
-        atributos.put(TipoAtributo.FECHA_NACIMIENTO, new Atributo(TipoAtributo.FECHA_NACIMIENTO, null));
+/**
+ * Método toString. Implementa una operación específica.
+ */
+    public String toString() {
+        return super.toString() + ", apellido='" + apellido + '\'' + ", fechaNacimiento='" + fechaNacimiento + '\'';
     }
     
 }
